433bf8446ab1203fd4d2a3a943e47b7006430bd0
BAWION <132302507+BAWION@users.noreply.github.com>
2024-01-06 15:01:16 +0300

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -8,30 +8,29 @@ import logging
 
 # Константы и инициализация
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-OPENAI_TOKEN = os.environ['OPENAI_TOKEN']  
+OPENAI_TOKEN = os.environ['OPENAI_TOKEN']
 CHANNEL_NAME = os.environ['CHANNEL_NAME']
 
 openai.api_key = OPENAI_TOKEN
-
 bot = Bot(TELEGRAM_TOKEN)
 updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
 # Логирование
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-# Функции для генерации контента
+# Функции для генерации контента  
 def generate_content():
   # код генерации контента
-  
+
 def generate_test_content():
-  return "Тестовый пост" 
+  return "Тестовый пост"
 
 # Функция публикации
 def publish_post(context):
   msg = context.job.context[0]
-  context.bot.send_message(chat_id=CHANNEL_NAME, text=msg)
+  context.bot.send_message(chat_id=CHANNEL_NAME, text=msg)  
 
-# Планировщик  
+# Планировщик
 scheduler = BlockingScheduler()
 scheduler.add_job(publish_post, 'interval', seconds=5, args=[generate_content()])
 
@@ -40,5 +39,5 @@ if __name__ == '__main__':
   test_msg = generate_test_content()
   context = updater.job_queue
   publish_post(context, test_msg)
-  
+
   scheduler.start()
